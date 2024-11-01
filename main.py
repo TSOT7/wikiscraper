e66fd5aac2974f10c71619a0f1c87abe1f23d571
TSOT7 <tommyma1234@gmail.com>
2024-11-01 17:27:24 -0400

    print language content func

--- main.py
+++ main.py
@@ -4,13 +4,21 @@ import wikipediaapi
 
 wiki_wiki = wikipediaapi.Wikipedia('MyProjectName (merlin@example.com)', 'en')
 
-page_py = wiki_wiki.page('Python_(programming_language)')
 
-# Define the page to fetch
-page = wiki_wiki.page("Python (programming language)")
 
-# Print the summary (introductory paragraph)
-print(page.summary)
 
-# Print full text of the page if needed
-print(page.text[:1000])  # First 1000 characters, for example
+# page_py = wiki_wiki.page('Python_(programming_language)')
+
+def print_language_content():
+    user_input = input("Enter a programming language\n")
+
+    # Define the page to fetch
+    program_page = wiki_wiki.page(f"{user_input} (programming language)")
+
+    # Print the summary (introductory paragraph)
+    print(program_page.summary)
+
+    # Print full text of the page if needed
+    print(program_page.text[:1000])  # First 1000 characters, for example
+
+print_language_content()
