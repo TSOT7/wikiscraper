77e7e1dd7bef92de94b7af3221ffa361272c3c4f
TSOT7 <tommyma1234@gmail.com>
2024-11-05 18:47:01 -0500

    print_main_screem()

--- main.py
+++ main.py
@@ -7,30 +7,53 @@ from rich.prompt import Prompt
 
 wiki_wiki = wikipediaapi.Wikipedia('MyProjectName (merlin@example.com)', 'en')
 
-user_input = input("Enter topic \n")
+def print_main_screen():
+    console = Console()
+    
+    console.clear()
+    console.rule("[bold red] Wikiscraper")
+    
+    console.print("""
+                  Welcome to Wikiscraper. A TUI application that lets 
+                  you interact with the wikipedia-api for a distraction free 
+                  Wikipedia experience. Made for terminal lovers!
+                  
+                  To get started...
+                  """)
+    
+    page_title = console.input("""
+                  Enter a topic you would like to explore. 
+                  Ex python, lofi, fishing \n
+                  """)
+    return page_title
 
-page = wiki_wiki.page(user_input)
 
-disambig_phrases = [
+def search_topic():
+    user_input = print_main_screen()
+    page = wiki_wiki.page(user_input)
+
+    disambig_phrases = [
         "may refer to",
         "most commonly refers to",
         "can refer to",
         "is a disambiguation page"
     ]
 
-if page.exists():
-    if any(phrase in page.summary.lower() for phrase in disambig_phrases):
-        print(f"'{user_input}' is a disambiguation page. Possible topics include:")
-        for link_title in page.links.keys():
-            print(f"- {link_title}")
+    if page.exists():
+        if any(phrase in page.summary.lower() for phrase in disambig_phrases):
+            print(f"'{user_input}' is a disambiguation page. Possible topics include:")
+            for link_title in page.links.keys():
+                print(f"- {link_title}")
+        else:
+            # if page is NOT an dismabigious page
+            print(f"Title: {page.title}")
+            print(f"Summary: {page.summary}")        
     else:
-        # if page is NOT an dismabigious page
-        print(f"Title: {page.title}")
-        print(f"Summary: {page.summary}")        
-else:
-    # if page does not exist
-    print(f"No page found for '{user_input}'.")
+        # if page does not exist
+        print(f"No page found for '{user_input}'.")
 
+
+search_topic()
 
 
 # page_py = wiki_wiki.page('Python_(programming_language)')
@@ -49,46 +72,6 @@ else:
 #     # # Print full text of the page if needed
 #     # print(program_page.text[:1000])  # First 1000 characters, for example
 
-# def print_main_screen():
-#     console = Console()
-    
-#     console.clear()
-#     console.rule("[bold red] Wikiscraper")
-    
-#     console.print("""
-#                   Welcome to Wikiscraper. A TUI application that lets 
-#                   you interact with the wikipedia-api for a distraction free 
-#                   Wikipedia experience. Made for terminal lovers!
-                  
-#                   To get started...
-#                   """)
-    
-#     page_title = console.input("""
-#                   Enter a topic you would like to explore. 
-#                   Ex python, lofi, fishing \n
-#                   """)
-#     return page_title
+
  
-# def is_disambiguation_page(page_title):
-#     page = wiki_wiki.page(page_title)
-     
-#     if not page.exist():
-#         non_exist_string = f"There is no page under the name {page_title}"
-#         return non_exist_string
-#     disambig_phrases = [
-#         "may refer to",
-#         "most commonly refers to",
-#         "can refer to",
-#         "is a disambiguation page"
-#     ]
-
-
-     
-
-    
-    
-    
-        
-        
-# print_language_content()
 
